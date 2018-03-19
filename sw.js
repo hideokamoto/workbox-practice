cf51d52705c88809ab5fb39ceb21dc7b45437f9b
hideokamoto <hide.okamoto@digitalcube.jp>
2018-03-20 01:02:26 +0900

    register css / image files

--- sw.js
+++ sw.js
@@ -10,4 +10,22 @@ function registerRoutes() {
     new RegExp('.*\.js'),
     workbox.strategies.networkFirst()
   )
+  workbox.routing.registerRoute(
+    /.*\.css/,
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: 'css-cache',
+    })
+  )
+  workbox.routing.registerRoute(
+    /.*\.(?:png|jpg|jpeg|svg|gif)/,
+    workbox.strategies.cacheFirst({
+      cacheName: 'image-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 20,
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+        })
+      ]
+    })
+  )
 }
