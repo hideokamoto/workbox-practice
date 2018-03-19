8af5da11ce4c4181279f271869ac30ba94da50d8
hideokamoto <hide.okamoto@digitalcube.jp>
2018-03-20 00:44:04 +0900

    cache js file as local

--- sw.js
+++ sw.js
@@ -1,6 +1,13 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 if (workbox) {
-  console.log('Yay! workbox is loaded')
+  registerRoutes()
 } else {
   console.log('Oh, workbox did not load')
+}
+
+function registerRoutes() {
+  workbox.routing.registerRoute(
+    new RegExp('.*\.js'),
+    workbox.strategies.networkFirst()
+  )
 }
