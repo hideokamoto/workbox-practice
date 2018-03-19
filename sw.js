e5c12e6a53ee206110ffc3fe6827c5d0b9ca12f1
hideokamoto <hide.okamoto@digitalcube.jp>
2018-03-20 00:27:36 +0900

    import workbox

--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
-console.log('hello from sw.js')
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+if (workbox) {
+  console.log('Yay! workbox is loaded')
+} else {
+  console.log('Oh, workbox did not load')
+}
